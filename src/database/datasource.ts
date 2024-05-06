e8fdd85e778da4d2a3bf95106bc2ab6af1e89f66
Renan Machado <renanpando@gmail.com>
2024-05-06 01:35:39 -0300

    build: add migrations and entities directory

--- src/database/datasource.ts
+++ src/database/datasource.ts
@@ -1,7 +1,6 @@
-// import { Pool } from "pg";
 import { config } from "dotenv";
 import { DataSource } from "typeorm";
-import { User } from "../models/user";
+//import { User } from "../models/user";
 
 config();
 
@@ -12,12 +11,15 @@ const dataSource = new DataSource({
   username: process.env.PGSQL_USER,
   password: process.env.PGSQL_PASSWORD,
   database: process.env.PGSQL_DB,
-  entities: [User],
+  entities: ["./src/models/**.ts"],
+  migrations: ["./src/migrations/**.ts"]
 });
 
 dataSource
   .initialize()
   .then(() => console.log("BANCO DE DADOS: Conectado!"))
-  .catch((error) => console.error("ERRO: Erro ao se conectar com o banco de dados!", error));
+  .catch((error) =>
+    console.error("ERRO: Erro ao se conectar com o banco de dados!", error)
+  );
 
 export default dataSource;
