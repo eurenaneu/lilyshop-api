92f9f6e416d62e12cd1af29b726d4e437a4def73
Renan Machado <renanpando@gmail.com>
2024-05-06 01:44:16 -0300

    feat(enum): create color enum

---
+++ src/enum/color.enum.ts
@@ -0,0 +1,11 @@
+export enum ColorName {
+    BRANCO = "Branco",
+    AMARELO = "Amarelo",
+    VERMELHO = "Vermelho",
+    AZUL = "Azul",
+    VERDE = "Verde",
+    LARANJA = "Laranja",
+    ROXO = "Roxo",
+    ROSA = "Rosa",
+    COLORIDA = "Colorida"
+}
