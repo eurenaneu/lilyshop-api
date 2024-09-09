59ab50288e238efe47cc11f87c364067477fd8da
Renan Machado <renanpando@gmail.com>
2024-09-09 10:49:42 -0300

    refactor: add new status

--- src/utils/httpresponse.ts
+++ src/utils/httpresponse.ts
@@ -2,8 +2,15 @@ export enum HttpStatus {
   OK = 200,
   CREATED = 201,
   NO_CONTENT = 204,
+  BAD_REQUEST = 400,
+  NOT_AUTHORIZED = 401,
+  PAYMENT_REQUIRED = 402,
+  FORBIDDEN = 403,
   NOT_FOUND = 404,
+  CONFLICT = 409,
   INTERNAL_SERVER_ERROR = 500,
+  BAD_GATEWAY = 502,
+  SERVICE_UNAVAILABLE = 503,
 }
 
 export class HttpResponse<T> {
