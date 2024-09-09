ea620ad337dc4b70ee50729875d680afbf1a2e7a
Renan Machado <renanpando@gmail.com>
2024-09-09 11:16:35 -0300

    delete old files

--- src/repositories/product/product.repository.ts
+++
@@ -1,4 +0,0 @@
-import dataSource from "../../database/datasource";
-import { Product } from "../../models/product";
-
-export const ProductRepository = dataSource.getRepository(Product);
--- src/repositories/user/user.repository.ts
+++
@@ -1,4 +0,0 @@
-import dataSource from "../../database/datasource";
-import { User } from "../../models/user";
-
-export const UserRepository = dataSource.getRepository(User);
