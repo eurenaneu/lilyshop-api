11a27a7b1e10042a1ab1c96ba15a98a26306bc06
Renan Machado <renanpando@gmail.com>
2024-09-09 10:39:27 -0300

    refactor(migrations): add datatypes and change column type

--- src/migrations/1713303750234-CreateProductTable.ts
+++ src/migrations/1713303750234-CreateProductTable.ts
@@ -1,17 +1,18 @@
-import { MigrationInterface, QueryRunner, Table, TableForeignKey } from "typeorm";
+import { MigrationInterface, QueryRunner, Table, TableCheck, TableForeignKey } from "typeorm";
 
 export class CreateProductTable1713303750234 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: "Products",
+                name: "product",
                 columns: [
                     {
-                        name: "id",
+                        name: "product_id",
                         type: "uuid",
                         isPrimary: true,
-                        isGenerated: true
+                        isGenerated: true,
+                        generationStrategy: "uuid"
                     },
                     {
                         name: "name",
@@ -20,40 +21,47 @@ export class CreateProductTable1713303750234 implements MigrationInterface {
                     },
                     {
                         name: "description",
-                        type: "varchar",
+                        type: "text",
                         isNullable: false
                     },
                     {
                         name: "color_id",
-                        type: "int",
+                        type: "integer",
                         isNullable: false
                     },
                     {
-                        name: "price",
-                        type: "numeric(5,2)",
+                        name: "unit_price",
+                        type: "numeric",
+                        precision: 5,
+                        scale: 2,
                         isNullable: false
                     },
                     {
                         name: "created_at",
-                        type: "timestamp without timezone",
-                        isNullable: false
+                        type: "timestamp without time zone",
+                        isNullable: false,
+                        default: "CURRENT_TIMESTAMP"
                     }
                 ]
-            })
-        )
+            }), true
+        );
 
         const foreignKey = new TableForeignKey({
             columnNames: ["color_id"],
-            referencedColumnNames: ["id"],
-            referencedTableName: "Colors",
+            referencedColumnNames: ["color_id"],
+            referencedTableName: "color",
             onDelete: "CASCADE"
-        })
+        });
 
-        await queryRunner.createForeignKey("Products", foreignKey);
+        await queryRunner.createForeignKey("product", foreignKey);
+        await queryRunner.createCheckConstraint("product", new TableCheck({
+            columnNames: ["unit_price"],
+            expression: "unit_price >= 0"
+        }));
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable("Products", true)
+        await queryRunner.dropTable("product", true)
     }
 
 }
